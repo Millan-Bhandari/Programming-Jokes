f4cc854b61424580ef4fc1ef306442f656a50c26
Millan Bhandari <millan.ib@icloud.com>
2022-07-26 11:19:24 -0500

    got API key from dad jokes

--- Programming Jokes/ContentView.swift
+++ Programming Jokes/ContentView.swift
@@ -25,12 +25,7 @@ struct ContentView: View {
     }
     
     func getJokes() {
-        jokes.append(Joke(setup: "Why do programmers always get Christmas and Halloween mixed up?",
-                          punchline: "Because DEC 25 = OCT 31"))
-        jokes.append(Joke(setup: "How did the programmer die in the shower?",
-                          punchline: "She followed the shampoo bottle instructions: Lather. Rinse. Repeat."))
-        jokes.append(Joke(setup: "There are 10 types of people in the world",
-                          punchline: "Those who understand binary and those who don’t."))
+        let apiKey = "?rapidapi-key=507601bd96msh7505a1cd41b591ep18cd14jsn0d9aa72d8d7f"
     }
 }
 
